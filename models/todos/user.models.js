4c9afa5c75a57dc6d1798d4d501214a9cbe45e41
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-02-09 23:41:25 +0530

    user model added

--- models/todos/user.models.js
+++ models/todos/user.models.js
@@ -1,7 +1,22 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-
-});
+const userSchema = new mongoose.Schema(
+{
+        username:{
+        type: String,
+        unique: true,
+        lowercase:true,
+        required:[true,"username is required"]
+        },
+        email: {
+            type: String,
+            required: [true,"email is required"]
+        },
+        password: {
+            type: String, 
+            required: [true, "password is required"],
+        }
+},    {timeStamps: true,}
+);
 
 export default mongoose.model("User",userSchema)
